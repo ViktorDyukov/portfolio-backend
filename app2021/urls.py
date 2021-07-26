494dac4a2409f986c8148021edf7bf704641e47f
Viktor Dyukov <spam@ducov.ru>
2021-07-27 02:26:21 +0300

    reloading

--- app2021/urls.py
+++ app2021/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 
 urlpatterns = [
-    path("cases/", views.CaseView.as_view()),
-    path("highlights/<int:link>/", views.LinkView.as_view()),
-    path("case/<int:id>/", views.CaseDetailView.as_view()),
-    path("page/<str:purl>/", views.PageView.as_view()),
+    path("api/cases/", views.CaseView.as_view()),
+    path("api/highlights/<int:link>/", views.LinkView.as_view()),
+    path("api/case/<int:id>/", views.CaseDetailView.as_view()),
+    path("api/page/<str:purl>/", views.PageView.as_view()),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
