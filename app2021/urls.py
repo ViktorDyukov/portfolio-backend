63eb9c71ce09d82045c1abfe467292a22bc3d06c
Viktor Dyukov <spam@ducov.ru>
2021-07-27 03:17:01 +0300

    reloading

--- app2021/urls.py
+++ app2021/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("api/cases_test/", views.CaseView.as_view()),
+    path("api/cases/", views.CaseView.as_view()),
     path("api/highlights/<int:link>/", views.LinkView.as_view()),
     path("api/case/<int:id>/", views.CaseDetailView.as_view()),
     path("api/page/<str:purl>/", views.PageView.as_view()),
