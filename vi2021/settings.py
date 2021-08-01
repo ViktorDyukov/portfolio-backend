31a875c87c8e7f55114afbbbccaa02475182c71f
Viktor Dyukov <spam@ducov.ru>
2021-08-01 16:54:41 +0300

    webp

--- vi2021/settings.py
+++ vi2021/settings.py
@@ -183,6 +183,36 @@ THUMBNAIL_ALIASES = {
     },
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/django.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
+
 PUB_CUSTOMIZATION = 2
 
 try:
