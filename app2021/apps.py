a168998434c341fb90b1b5071a0cd09f6feefd95
Viktor Dyukov <spam@ducov.ru>
2021-08-01 18:11:39 +0300

    logging

--- app2021/apps.py
+++ app2021/apps.py
@@ -13,5 +13,6 @@ class App2021Config(AppConfig):
     name = 'app2021'
 
     def ready(self):
+        logger.error("222333")
         from easy_thumbnails.signals import thumbnail_created
         thumbnail_created.connect(store_as_webp)
